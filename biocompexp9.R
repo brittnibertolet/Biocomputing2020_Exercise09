025b1f9dd9ad814b79ae5415591ccada1715d05d
kmichael4324 <71276752+kmichael4324@users.noreply.github.com>
2020-10-28 14:42:03 -0400

    Update biocompexp9.R
    warning and functionality to override added

--- biocompexp9.R
+++ biocompexp9.R
@@ -7,14 +7,22 @@ getcovar <- function(dir, yourcolumn){
   for(i in dir){
     yfile=read.csv(file=i, header=F, sep = ",")
     covar= (sd(yfile[,yourcolumn]))/(mean(yfile[,yourcolumn]))
-    c(allcovars, covar)
+    if(nrow(yfile<50)) {
+      override = readline(prompt = "There are less than 50 observations, would you like to continue (Y/N)? ")
+      if(override =="Y"){
+        allcovars <- c(allcovars, covar)
+      }
+      else{
+        next 
+      }
+    }
+    else{
+      c(allcovars, covar)
+    }
   }
   return(allcovars)
 }
-  
+
 
 your_dir <- list.files(path = "/Users/kevinmichael/Desktop/BioComp/Test")
 getcovar(dir=your_dir, yourcolumn = 3)
-
-
-
