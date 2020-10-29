1271eb68ca7e16d1d23a2cdfa219782599ea038f
ilickona0 <ilickona@gmail.com>
2020-10-29 19:03:09 -0400

    got the loop to read the files and calculate the CV working

--- Dir_Function.R
+++ Dir_Function.R
@@ -26,20 +26,20 @@ cofunc <- function(dir) {
   message
   usercol <- scan(nmax=1)
   #Read in the files using the names from the filenames vector
-  filedata <- filenames
+  filedata <- 1:length(filenames)
   for(i in 1:length(filenames)){
     #get file name as a character"
     conc <- c(dir,filenames[i])
     concy <- paste0(conc[1],conc[2])
     #Read file and store in temporary variable that will be overwritten
-    tempvar <- read.csv(paste0(conc[1],conc[2]), header=FALSE)
+    tempvar <- read.csv(paste0(conc[1],conc[2]), header=TRUE)
     
     #Perform calculation on specified column usercol
     avg = mean(tempvar[,usercol])
     stdv = sd(tempvar[,usercol])
     cv <- stdv/avg
     
-    
+    filedata[i] <- cv
     
   }
 }
