517e73d6188b6e6faccbfd29fcf2689a02832cd6
ilickona0 <ilickona@gmail.com>
2020-10-29 18:33:59 -0400

    added intial paramters and beginning of a for loop to load files

--- Dir_Function.R
+++ Dir_Function.R
@@ -5,4 +5,25 @@
 setwd("/Users/isaaclickona/tut9/Biocomputing2020_Exercise09")
 
 
-dir = 
+dir = "/Users/isaaclickona/testerdirectory/"
+
+cofunc <- function(dir) {
+  #First, find all the files in the directory and their names
+  filenames = list.files(dir)
+  filenames
+  
+  #Get user specified column 
+  message <- "Please choose which column to perform calculations on"
+  message
+  usercol <- scan(nmax=1)
+  #Read in the files using the names from the filenames vector
+  filedata <- filenames
+  for(i in 1:length(filenames)){
+    #get file name as a character"
+    conc <- c(dir,filenames[i])
+    concy <- paste0(conc[1],conc[2])
+    tempvar <- read.csv(paste0(conc[1],conc[2]), header=FALSE)
+    filedata[i] <- tempvar
+    
+  }
+}
