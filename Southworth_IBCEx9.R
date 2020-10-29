e775d315abeb1872cfc6d3abadd339654b6f60e8
chelsea307 <71391201+chelsea307@users.noreply.github.com>
2020-10-29 11:55:56 -0400

    Add files via upload

--- Southworth_IBCEx9.R
+++ Southworth_IBCEx9.R
@@ -13,11 +13,38 @@
 # Then, do this for each file in a specified directory
 # Directory name is an argument called 'dir', plus any other arguments needed for the task
 
-# 1. Create some fake data with >50 rows, no NAs 
+# 1. Create some fake data with: >50 rows, no NAs; <50 rows, no NAs; >50 rows, NAs; <50 rows, NAs 
 # 2. Code to read the data file into R
+library(readr)
+Ex9Data <- read_csv("~/Desktop/Ex9Data.csv")
+View(Ex9Data)
 # 3. Function to calculate coefficient of variation for a specified column
-# 4. Create some fake data with <50 rows (and read it into the code from 2 & 3)
-# 5. Tell R to give an error if <50 rows, but allow this error to be overriden with user input
-# 6. Create fake data with NAs (<50 and <50 rows) -- see what happens in R
+  # calculate standard deviation - works
+sd(Ex9Data$more_noNA)
+  # calculate mean - works
+mean(Ex9Data$more_noNA)
+  # divide sd by mean to give coefficient of variation
+sd(Ex9Data$more_noNA)/mean(Ex9Data$more_noNA)
+
+# assign desired column to "x"
+x <- Ex9Data$more_noNA
+# save CV calculations as a function
+CV <- (sd(x)/mean(x))
+CV
+
+# 4. Use <50 rows
+  # Tell R to give an error if <50 rows, but allow this error to be overriden with user input
+  # use warning() to generate a warning that can be overriden
+
+
+# 5. See what happens in R with NA rows
   # then troubleshoot and figure out how to correct it
+
+
+CV <- function(x){
+  (sd(x)/mean(x))*100
+}
+
+sd(Ex9Data$more_noNA)
+mean(Ex9Data$more_noNA)
 
