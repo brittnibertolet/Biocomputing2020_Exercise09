aa37f4534d87f59f7d97fc72ae10da94eba53a78
kmichael4324 <71276752+kmichael4324@users.noreply.github.com>
2020-10-29 14:20:25 -0400

    Update biocompexp9.R
    Final version

--- biocompexp9.R
+++ biocompexp9.R
@@ -12,7 +12,7 @@ getcolumncovar <- function(dir, yourcolumn){
     #now we take the second user input, yourcolumn, and find the SD/Mean of column that the user inputs a number for
     covar= (sd(yourfile[,yourcolumn]))/(mean(yourfile[,yourcolumn]))
     #including an if statement for when there are less than 50 observations in a file
-    if(nrow(yfile<50)) {
+    if(nrow(yourfile<50)) {
       #if there are, the user is prompted with an override
       #if they choose Y for the override, the file is included, and if not this file is skipped
       override = readline(prompt = "There are less than 50 observations, would you like to continue (Y/N)? ")
@@ -20,11 +20,12 @@ getcolumncovar <- function(dir, yourcolumn){
         #we add obersvations to final vector by combining the vector with the current files coefficient of variation
         allcovars <- c(allcovars, covar)
       }
+      #if the override is not a Y, we skip thi observation with next 
       else{
         next 
       }
     }
-    #if there are more than 50 observations, we simply just add the files CoV to the final allcovars vector 
+    #if nrows greater than 50, we simply append the covar of that file to the allcovars vector
     else{
       allcovars <- c(allcovars, covar)
     }
@@ -35,4 +36,4 @@ getcolumncovar <- function(dir, yourcolumn){
 #specify the path of your directory 
 your_dir <- list.files(path = "/Users/kevinmichael/Desktop/BioComp/Test")
 #now call the function with your_dir as the directory of choice and which ever column the user wants
-getcolumncovar(dir=your_dir, yourcolumn = 3)
+getcolumncovar(dir=your_dir, yourcolumn = 2)
