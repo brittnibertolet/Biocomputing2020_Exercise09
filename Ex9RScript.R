0a66edcb9ef43e4de71fec191cf5779a1a96fd25
Michelle Huang <o.omichelley@gmail.com>
2020-10-29 22:24:39 -0400

    is it working?

--- Ex9RScript.R
+++ Ex9RScript.R
@@ -5,6 +5,10 @@
 ## set default of 50 obs, report an error if fewer than 50
 ## function includes NA data
 
+files <- c()
+lst <- vector("list", length(files))
+names(lst) <- files
+
 coeffvar <- function(d, colname){
   files <- dir(d, pattern = "*.csv")
   for (i in 1:nrow(files)){
@@ -18,9 +22,21 @@ coeffvar <- function(d, colname){
 
 coeffvar("~/Desktop/Bioinformatics/Biocomputing2020_Exercise09/", observations)
 
-test <- c(1,2,3,4,5,6,7,8,9,10,11,10,9,8,7,6,5,4,3,2,1)
-mean(test)
-sd(test)
-sd(test)/mean(test)
+# ^ that didn't quite work...
+# attempt 2
+
 files <- dir("~/Desktop/Bioinformatics/Biocomputing2020_Exercise09/", pattern="*.csv")
-dim(files)
+lst <- vector("list", length(files))
+names(lst) <- files
+
+coeffvar <- function(d, colname){
+  for (i in 1:length(files)){
+  tmp <- read.csv(files[i], stringsAsFactors = FALSE)
+  if (length(i$colname) < 50){
+    print("ERROR; file has fewer than 50 observations")
+  } else {
+    sd(i$colname)/mean(i$colname)
+  }
+  }
+}
+coeffvar("~/Desktop/Bioinformatics/Biocomputing2020_Exercise09/",observations)
