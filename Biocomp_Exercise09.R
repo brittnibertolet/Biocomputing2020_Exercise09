765334287a42ef38adcc102dd09428962a3cfcaa
Katrina Fliotsos <kfliotso@nd.edu>
2020-10-30 01:33:24 -0400

    added comments to code and na portion

--- Biocomp_Exercise09.R
+++ Biocomp_Exercise09.R
@@ -2,10 +2,11 @@ setwd("~/Desktop/data/Biocomputing2020_Exercise09/")
 #writing a custom function with dir as argument
 
 #directory name is the argument "dir"
-fun1= function(dir,col){
+fun1= function(dir,col){ #calling the specific directory and specific column from files
   list.files(dir)  #listing all the files in the directory
-  read.table(dir)
-  dir[,col]
+  read.table(dir) #reading in files in directory
+  is.na(dir)=F #making sure there's no NA in file
+  dir[,col] #specifying work on a specific column
   if(nrow(dir)<50)  #producing the warning message, will return
   {warning("There are less than 50 observations")}
   else{
@@ -16,7 +17,8 @@ fun1= function(dir,col){
   CoeffVar<-Stdev/Mean #calculating coeffvar
   return(CoeffVar)
 }
-  
+
+
 #creating test files
 col1<-c(1,26,5,3,3,5,7,34,23,4,3,45,34)
 col2<-c(2,3,4,1,3,4,65,7,8,3,423,765,23)
